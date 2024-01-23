f3259a19abc531f0165bc1bc552a41e14ff887ca
joaovitorscr <98563963+joaovitorscr@users.noreply.github.com>
2024-01-23 15:25:49 -0300

    FEAT: Shadcn/ui for dark mode and ThemeProver wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import Header from './components/Header'
+import Container from '@/components/Container'
+import Header from '@/components/Header'
 
 function App() {
   return (
     <main>
       <Header />
-      <h2>Init</h2>
+      <Container>
+        <section id="about" className="h-[calc(100vh-64px)]">
+          <article className="flex flex-col items-center justify-center h-full">
+            <h2>João Vitor Santos Cruz</h2>
+            <div className="max-w-80">
+              <p>
+                Started in front-end development at the end of 2022. Since then
+                I have learned several technologies like React, TypeScript,
+                Next.js, TailwindCSS and others...
+              </p>
+              <p>
+                I&apos;m currently learning more about back-end and node.js
+                frameworks so I can become a full-stack developer in the future.
+              </p>
+            </div>
+          </article>
+        </section>
+        <section id="Skills">
+          <h2>Skills</h2>
+        </section>
+      </Container>
     </main>
   )
 }
