4c2773fc7965bfed435053d0ff9525c1d7d2408e
joaovitorscr <98563963+joaovitorscr@users.noreply.github.com>
2025-01-16 22:19:48 -0300

    FEAT: Add posthog

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,18 @@ import { ThemeProvider } from './components/ThemeProvider.tsx'
 import './app/index.css'
 import './i18n.ts'
 
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.POSTHOG_HOST,
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <App />
+      <PostHogProvider apiKey={import.meta.env.POSTHOG_KEY} options={options}>
+        <App />
+      </PostHogProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly POSTHOG_KEY: string
+  readonly POSTHOG_HOST: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
