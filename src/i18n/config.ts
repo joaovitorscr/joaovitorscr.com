cf595ed517bb3162d482521e14c41fc941d6aee4
joaovitorscr <98563963+joaovitorscr@users.noreply.github.com>
2026-02-01 22:19:35 -0300

    FEAT: Add translations for spanish

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
 export type Locale = (typeof locales)[number];
 
-export const locales = ["en", "pt"] as const;
+export const locales = ["en", "pt", "es"] as const;
 export const defaultLocale: Locale = "en";
